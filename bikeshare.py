0f601ffbdfa3aa7040de30916ea7fb613f76c8e9
Tem Strongbert Bama <tembamastrongbert@gmail.com>
2020-02-17 17:05:36 +0100

    Improve repeat loop in main function

--- bikeshare.py
+++ bikeshare.py
@@ -380,7 +380,7 @@ def main():
         user_stats(df)
         trip_data(df)
         
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Yes or No.\n')
         if restart.lower() != 'yes':
             break
 
